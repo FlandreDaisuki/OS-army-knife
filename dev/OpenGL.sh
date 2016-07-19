6a058593d7e0c46ea49e343b7d84dfff70b427b1
FlandreDaisuki <vbnm123c@gmail.com>
2016-07-19 18:53:27 +0800

    add dev

---
+++ dev/OpenGL.sh
@@ -0,0 +1,5 @@
+sudo apt-get install build-essential \
+libgl1-mesa-dev \
+freeglut3 \
+freeglut3-dev \
+python-opengl
---
+++ dev/pip.sh
@@ -0,0 +1,2 @@
+pip install pip --upgrade && \
+pip install pyOpenGL PyOpenGL-accelerate pillow numpy
