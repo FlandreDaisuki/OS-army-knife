4c82b690826b2576c61088fe6d493d5a93554668
FlandreDaisuki <vbnm123c@gmail.com>
2016-07-19 18:54:27 +0800

    add core.editor

--- essentials/ssh_and_github.sh
+++ essentials/ssh_and_github.sh
@@ -3,6 +3,7 @@ ssh-keygen -t rsa -b 4096
 
 git config --global user.name "FlandreDaisuki"
 git config --global user.email "vbnm123c@gmail.com"
+git config --global core.editor "vim"
 
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
