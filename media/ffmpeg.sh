47035c632aad3cfb92c4950b4f00eb64e119183d
FlandreDaisuki <vbnm123c@gmail.com>
2016-07-21 03:39:26 +0800

    add media/ffmpeg.sh

---
+++ media/ffmpeg.sh
@@ -0,0 +1,20 @@
+# https://ffmpeg.org/download.html
+
+cat /etc/os-release
+osname=`cat /etc/os-release | grep NAME | head -n1 | cut -d \" -f 2`
+vid=`cat /etc/os-release | grep VERSION_ID | cut -d \" -f 2 | sed 's/\.//g'`
+
+#if Ubuntu
+if [ $osname == "Ubuntu" ]; then
+    if [ $((vid)) -lt 1404 ]; then
+        echo "your Ubuntu version too old"
+    elif [ $((vid)) -eq 1404 ]; then
+        # Ubuntu – Ubuntu Multimedia for Trusty PPA. Provides static binaries from most recent release branch.
+        sudo add-apt-repository ppa:mc3man/trusty-media
+        sudo apt-get update
+        sudo apt-get install -y ffmpeg
+    else
+        # Ubuntu – Official packages for Vivid, Wily, Xenial
+        sudo apt-get install -y ffmpeg
+    fi
+fi
